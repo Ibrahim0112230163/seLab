3679f13bc3e1cbd8da94b0766773710a6fdab2c9
Ibrahim <ibrahimsohan04@gmail.com>
2026-01-06 02:48:20 +0600

    7th commit and added a py file

---
+++ q1_sum_ranges.py
@@ -0,0 +1,16 @@
+# Question 1: Sum of given ranges
+sum1 = 0
+for i in range(2, 21):
+    sum1 += i
+
+sum2 = 0
+for i in range(11, 101):
+    sum2 += i
+
+sum3 = 0
+for i in range(50, 501):
+    sum3 += i
+
+total = sum1 + sum2 + sum3
+print("Total sum is:", total)
+
